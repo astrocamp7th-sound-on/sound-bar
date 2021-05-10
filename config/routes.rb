c22389ee1ac64c6d885875516aafb3ea5ae36297
Lailai-lenovo <prettyfighty@gmail.com>
2021-05-10 16:58:37 +0800

    Podcast CRUD finished & delete column user_id

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :podcasts do
+    resources :episodes, shallow: true, except: [:index] do
+      resources :comments, shallow: true, only: [:create]
+    end
+  end
+
+
 end
