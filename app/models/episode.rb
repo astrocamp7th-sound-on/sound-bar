7a3012557a873e835e05e94475a04b50f92a822d
Alex Chuang <ball16357@gmail.com>
2021-05-14 10:57:05 +0800

    安裝Active Storage

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,6 +1,8 @@
 class Episode < ApplicationRecord
   belongs_to :podcast
   has_many :comments
-  mount_uploader :cover, CoverUploader
+  # mount_uploader :cover, CoverUploader
+
+  has_one_attached :audio
 
 end
