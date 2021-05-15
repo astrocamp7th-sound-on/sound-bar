c454666b012680f545e95ad315c605806526e97b
Alex Chuang <ball16357@gmail.com>
2021-05-15 16:29:14 +0800

    rails g migration add_recording_to_episodes recording:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_14_024804) do
+ActiveRecord::Schema.define(version: 2021_05_14_082251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2021_05_14_024804) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "episode_cover"
+    t.string "recording"
     t.index ["podcast_id"], name: "index_episodes_on_podcast_id"
   end
 
