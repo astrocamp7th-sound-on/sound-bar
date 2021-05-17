ca95e0242db030fca43796c395fad029a48f4952
sheng87 <4004171022kyu@gmail.com>
2021-05-17 11:57:30 +0800

    set omniauth provider [facebook,google]

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_05_11_031906) do
-
-
+ActiveRecord::Schema.define(version: 2021_05_17_025047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +90,10 @@ ActiveRecord::Schema.define(version: 2021_05_11_031906) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "fb_uid"
+    t.string "fb_token"
+    t.string "google_uid"
+    t.string "google_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
