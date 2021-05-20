e3e7acac2a5f1f1f7403817e67e282d97aa1fdce
sheng87 <4004171022kyu@gmail.com>
2021-05-20 20:57:39 +0800

    i18n gem remove and modify existing_user to registered_user in user.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,8 +9,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
 
     def callback(provider)
-        @user = User.from_omniauth(request.env["omniauth.auth"])
-  
+      @user = User.from_omniauth(request.env["omniauth.auth"])
       if @user.persisted?
         sign_in_and_redirect @user, event: :authentication 
         set_flash_message(:notice, :success, kind: "#{provider}") if is_navigational_format?
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,31 +11,33 @@ class User < ApplicationRecord
   has_many :subscriptions
   has_many :comments
 
-  def self.from_omniauth(access_token, signed_in_resource=nil)
+
+
+# ref:https://cindyliu923.medium.com/rails-devise-google-fecebook%E7%99%BB%E5%85%A5%E5%AF%A6%E4%BD%9C-ebfb3170b0a8
+
+  def self.from_omniauth(access_token)
     data = access_token.info
-    user = User.where(:token => access_token.credentials.token, :uid => access_token.uid,:username => access_token.info.name ).first    
+    user = User.where(:token => access_token.credentials.token, :uid => access_token.uid).first    
     if user
       return user
     else
-      existing_user = User.where(:email => data["email"]).first
-      if  existing_user
-        existing_user.username = access_token.info.name
-        existing_user.uid = access_token.uid
-        existing_user.token = access_token.credentials.token
-        existing_user.save!
-        return existing_user
+      # 已使用devise註冊後用第三方登入
+      registered_user = User.where(:email => data["email"]).first
+      if registered_user
+        registered_user.username = access_token.info.name
+        registered_user.uid = access_token.uid
+        registered_user.token = access_token.credentials.token
+        registered_user.save!
+        return registered_user
       else
-    
         user = User.create(
-            username: access_token.info.name,
-            email: data["email"],
-            password: Devise.friendly_token[0,20],
-            token: access_token.credentials.token,
-            uid: access_token.uid
-          )
+          username: access_token.info.name,
+          email: data["email"],
+          password: Devise.friendly_token[0,20],
+          token: access_token.credentials.token,
+          uid: access_token.uid
+        )
       end
     end
   end
-
-end  
-# ref:https://cindyliu923.medium.com/rails-devise-google-fecebook%E7%99%BB%E5%85%A5%E5%AF%A6%E4%BD%9C-ebfb3170b0a8
+end
