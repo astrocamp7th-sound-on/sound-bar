ede82b8a433149f0716bdfb64562d39abe659713
lailai <prettyfighty@gmail.com>
2021-05-20 17:47:16 +0800

    organize codes

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -51,16 +51,7 @@ class PodcastsController < ApplicationController
     @donation = @podcast.donations.new(donation_params)
 
     if @donation.save
-      trade_date = Time.now.strftime("%Y/%m/%d %H:%M:%S")
-      trade_no = Time.now.strftime("%y%m%d#{[*1..100].sample%100/10.floor}%3N#{[*1..100].sample%10}")
-      @donation.update(tradeno: "#{trade_no}")
-      hash_params = "HashKey=5294y06JbISpM5x9&ChoosePayment=Credit&ClientBackURL=http://localhost:3000/podcasts&EncryptType=1&ItemName=贊助#{@podcast.name}&MerchantID=2000132&MerchantTradeDate=#{trade_date}&MerchantTradeNo=#{trade_no}&PaymentType=aio&ReturnURL=http://localhost:3000&TotalAmount=#{@donation.amount}&TradeDesc=soundbar_donate&HashIV=v77hoKGq4kWxNNIS"
-      mac_value = (Digest::SHA256.hexdigest url_encode(hash_params).gsub("%20","+").downcase).upcase
-      
-      ec_url = "https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5"
-      ec_params = { ChoosePayment: "Credit", ClientBackURL: "http://localhost:3000/podcasts", EncryptType: "1", ItemName: "贊助#{@podcast.name}", MerchantID: "2000132", MerchantTradeDate: "#{trade_date}", MerchantTradeNo: "#{trade_no}", PaymentType: "aio", ReturnURL: "http://localhost:3000", TotalAmount: "#{@donation.amount}", TradeDesc: "soundbar_donate", CheckMacValue: "#{mac_value}" }
-      
-      repost(ec_url, params: ec_params) if @donation.save
+      connect_ecpay
     else
       render :new_donation
     end
@@ -68,6 +59,10 @@ class PodcastsController < ApplicationController
 
 
   private
+  def find_podcast
+    @podcast = Podcast.find(params[:id])
+  end
+
   def podcast_params
     params.require(:podcast).permit(:avatar, :name, :artist, :email, :language, :slug, :genres, :description, :subtitle, :weblink, :copyright, :explicit, :status, :cover)
   end
@@ -76,7 +71,16 @@ class PodcastsController < ApplicationController
     params.require(:donation).permit(:donator, :note, :amount)
   end
 
-  def find_podcast
-    @podcast = Podcast.find(params[:id])
+  def connect_ecpay
+    trade_date = Time.now.strftime("%Y/%m/%d %H:%M:%S")
+    trade_no = Time.now.strftime("%y%m%d#{[*1..100].sample%100/10.floor}%3N#{[*1..100].sample%10}")
+    @donation.update(tradeno: "#{trade_no}")
+    hash_params = "HashKey=5294y06JbISpM5x9&ChoosePayment=ALL&ClientBackURL=http://localhost:3000/podcasts&EncryptType=1&ItemName=贊助節目：#{@podcast.name}&MerchantID=2000132&MerchantTradeDate=#{trade_date}&MerchantTradeNo=#{trade_no}&PaymentType=aio&ReturnURL=http://localhost:3000&TotalAmount=#{@donation.amount}&TradeDesc=soundbar_donate&HashIV=v77hoKGq4kWxNNIS"
+    mac_value = (Digest::SHA256.hexdigest url_encode(hash_params).gsub("%20","+").downcase).upcase
+    
+    ec_url = "https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5"
+    ec_params = { ChoosePayment: "ALL", ClientBackURL: "http://localhost:3000/podcasts", EncryptType: "1", ItemName: "贊助節目：#{@podcast.name}", MerchantID: "2000132", MerchantTradeDate: "#{trade_date}", MerchantTradeNo: "#{trade_no}", PaymentType: "aio", ReturnURL: "http://localhost:3000", TotalAmount: "#{@donation.amount}", TradeDesc: "soundbar_donate", CheckMacValue: "#{mac_value}" }
+    
+    repost(ec_url, params: ec_params)
   end
 end
