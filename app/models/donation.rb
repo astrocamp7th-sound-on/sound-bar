51f6abac0def4f2780f0dec47b55ed0f9d51611a
lailai <prettyfighty@gmail.com>
2021-05-21 13:31:17 +0800

    delete unnecessary codes and file

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,6 +1,6 @@
 class Donation < ApplicationRecord
   belongs_to :podcast
-  validates :amount, numericality: { only_integer: true, greater_than: 50, allow_nil: false }
+  validates :amount, numericality: { only_integer: true, greater_than: 49, allow_nil: false }
 
   include AASM
 
