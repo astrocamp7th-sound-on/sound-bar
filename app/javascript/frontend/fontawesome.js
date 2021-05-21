e420307d86759b50a0a39537e91a87a2808ace94
Alex Chuang <ball16357@gmail.com>
2021-05-21 16:35:38 +0800

    delete and change js file name

--- app/javascript/frontend/fontawesome.js
+++
@@ -1,8 +0,0 @@
-import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faGlobe, faCog } from '@fortawesome/free-solid-svg-icons'
-
-import { faUser } from '@fortawesome/free-regular-svg-icons'
-
-
-library.add(faUser, faGlobe, faCog)
-dom.watch()
