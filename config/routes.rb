ff11f6a973a8955d1a8ee91a9aa68f3d1dbc4f8a
lailai <prettyfighty@gmail.com>
2021-05-21 13:08:13 +0800

    add path to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
 
-  resources :podcasts do
+  resources :podcasts, path: '/podcaster/podcasts' do
+    resources :episodes do
+    end
+  end
+
+  resources :p, only: [:index, :show] do
+    collection do
+      post '/donate_outcome', to: 'podcasts#donate_outcome'
+    end
     member do
       get '/donate', to: 'podcasts#new_donation'
       post '/donate', to: 'podcasts#donate!'
     end
-    resources :episodes do
+    resources :e, only: [:show] do
       resources :comments, shallow: true, only: [:create]
     end
   end
+
 
 
 
