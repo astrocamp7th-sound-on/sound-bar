709b4dbf4c177ecf727bd6a50b91cbac4a50e782
Lailai <prettyfighty@gmail.com>
2021-05-21 19:02:53 +0800

    rails g controller e & p

--- db/migrate/20210517043800_add_token_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddTokenToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :fb_uid, :string
-    add_column :users, :fb_token, :string
-    add_column :users, :google_uid, :string
-    add_column :users, :google_token, :string
-  end
-end
