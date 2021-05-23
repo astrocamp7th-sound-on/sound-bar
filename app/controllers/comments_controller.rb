1e0d9aa726c0f854dff003452cc0dba97ed6a513
Ray Hsu <ray199185@gmail.com>
2021-05-23 17:17:31 +0800

    comment finished

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: :create
-  skip_before_action :verify_authenticity_token
 
   def create
     @episode = Episode.find(params[:e_id])
@@ -11,7 +10,7 @@ class CommentsController < ApplicationController
 
     if @comment.save
       # 執行 create.js.erb
-      redirect_to p_e_path(@podcast, @episode)
+      # redirect_to p_e_path(@podcast, @episode)
     else
       render 'e/show'
     end
