d3a4a6581fb954cb2836eb5dd7bdedf2143c86ab
lailai <prettyfighty@gmail.com>
2021-05-28 10:20:07 +0800

    刪除console.log

--- app/javascript/host/index.js
+++ app/javascript/host/index.js
@@ -64,7 +64,6 @@ document.addEventListener('turbolinks:load', function() {
   // 使用者登入後的節目列表搜尋框
   if (searchForm){
     searchForm.addEventListener('focusin', (e) => {
-      console.log(e)
       e.target.parentElement.classList.add('search-bar-ring');
     })
     searchForm.addEventListener('focusout', (e) => {
