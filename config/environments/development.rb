dfea9a7314fc61b3d771a50f72acc2b078fcf33d
sheng87 <4004171022kyu@gmail.com>
2021-05-30 21:11:10 +0800

    audio column being removed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   # ngrok server
-  config.hosts << "c1a7a186639b.ngrok.io"
+  config.hosts << "1df5bbef92e8.ngrok.io"
   # subdomains
   config.action_dispatch.tld_length = 0
 end
