93da86f341b92990a5164b2e068e7db9fb420f8b
lailai <prettyfighty@gmail.com>
2021-06-02 10:46:14 +0800

    self reference model setting

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :episode
   belongs_to :user
+  has_many :replies, class_name: 'Comment', foreign_key: 'comment_id'
+  belongs_to :original_comment, class_name: 'Comment', foreign_key: 'comment_id', optional: true
 end
