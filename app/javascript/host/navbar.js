55f8ce0a5f3d1dabbcde94209fe45656b44b48e8
lailai <prettyfighty@gmail.com>
2021-06-04 13:30:06 +0800

    navbar搜尋功能完成 & 修正留言N+1問題

--- app/javascript/host/navbar.js
+++ app/javascript/host/navbar.js
@@ -1,10 +1,37 @@
+import Rails from "@rails/ujs"
+
 document.addEventListener("turbolinks:load", function () {
 
   let podcastNameInput = document.querySelector('#podcastNameInput')
 
+  // navbar搜尋節目功能
   if (podcastNameInput) {
-    podcastNameInput.addEventListener('input', function(e){
-      let podcastNameUl = document.querySelector('#podcastNameUl')
+    let timeout = null
+    podcastNameInput.addEventListener('keyup', function(e){
+      let searchValue = e.target.value
+      clearTimeout(timeout);
+      timeout = setTimeout(function () {
+        Rails.ajax({
+          url: '/api/v1/podcasts',
+          type: 'get',
+          success: function(res){
+            let result = res.filter(function(podcast){
+              return podcast.name.indexOf(searchValue) > -1
+            })
+            let content = result.map(function(podcast){
+              return `
+              <li class="my-1 hover:bg-gray-100 p-2">
+                <a href="/podcasts/${podcast.random_url}/dashboard" class="block">${podcast.name}</a>
+              </li>
+              `
+            })
+            document.querySelector('#podcastNameUl').innerHTML = content.join('')
+          },
+          failure: function(result){
+            console.log(result)
+          }
+        })
+      }, 1000);
     })
   }
 
