ac4a88b9ae561848aab11bb49a166f9b809c12a6
Lailai <prettyfighty@gmail.com>
2021-06-05 18:12:08 +0800

    新增編集Episode表單驗證完成

--- app/javascript/host/podcasts_index.js
+++ app/javascript/host/podcasts_index.js
@@ -20,36 +20,48 @@ document.addEventListener("turbolinks:load", function () {
   let fPodcastGenres = document.querySelector('#podcast_genres')
   let fPodcastDescription = document.querySelector('#podcast_description')
   let fPodcastCopyright = document.querySelector('#podcast_copyright')
+  let newEpisodeForm = document.querySelector('#new_episode')
+  let editEpisodeForm = document.querySelector('[id^="edit_episode"]')
+  let fEpisodeTitle = document.querySelector('#episode_title')
+  let fEpisodeDescription = document.querySelector('#episode_description')
+
+  // 驗證表單 必填
+  function validateInputPresence (e) {
+    let fErrorSpan = document.createElement('SPAN')
+    fErrorSpan.classList.add('error')
+    fErrorSpan.textContent = '必填'
+    if (e.target.value == '' && e.target.classList.toString().indexOf('border-red-400') == -1){
+      e.target.classList.add('border-red-400')
+      e.target.parentElement.appendChild(fErrorSpan)
+    } else if (e.target.value != '' && e.target.classList.contains('border-red-400')) {
+      e.target.classList.remove('border-red-400')
+      e.target.parentElement.removeChild(e.target.parentElement.lastElementChild)
+    }
+  }
+  
+  // 驗證表單 Email 格式
+  function validateInputEmail (e) {
+    let emailReg = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)
+    let fEmailErrorSpan = document.createElement('SPAN')
+    fEmailErrorSpan.classList.add('error')
+    fEmailErrorSpan.textContent = '請輸入正確的Email格式'
+    if (emailReg.test(e.target.value) == false && e.target.classList.toString().indexOf('border-red-400') == -1){
+      e.target.classList.add('border-red-400')
+      e.target.parentElement.appendChild(fEmailErrorSpan)
+    } else if (emailReg.test(e.target.value) == true && e.target.classList.contains('border-red-400')){
+      e.target.classList.remove('border-red-400')
+      e.target.parentElement.removeChild(e.target.parentElement.lastElementChild)
+    }
+  }
+
+  // 新增 Episode & 編輯 Episode 表單驗證
+  if (newEpisodeForm || editEpisodeForm){
+    fEpisodeTitle.addEventListener('blur', (e) => validateInputPresence(e))
+    fEpisodeDescription.addEventListener('blur', (e) => validateInputPresence(e))
+  }
 
   // 建立 Podcast & 編輯 Podcast 表單驗證
   if (newPodcastForm || editPodcastForm){
-
-    function validateInputPresence (e) {
-      let fErrorSpan = document.createElement('SPAN')
-      fErrorSpan.classList.add('error')
-      fErrorSpan.textContent = '必填'
-      if (e.target.value == '' && e.target.classList.toString().indexOf('border-red-400') == -1){
-        e.target.classList.add('border-red-400')
-        e.target.parentElement.appendChild(fErrorSpan)
-      } else if (e.target.value != '' && e.target.classList.contains('border-red-400')) {
-        e.target.classList.remove('border-red-400')
-        e.target.parentElement.removeChild(e.target.parentElement.lastElementChild)
-      }
-    }
-
-    function validateInputEmail (e) {
-      let emailReg = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)
-      let fEmailErrorSpan = document.createElement('SPAN')
-      fEmailErrorSpan.classList.add('error')
-      fEmailErrorSpan.textContent = '請輸入正確的Email格式'
-      if (emailReg.test(e.target.value) == false && e.target.classList.toString().indexOf('border-red-400') == -1){
-        e.target.classList.add('border-red-400')
-        e.target.parentElement.appendChild(fEmailErrorSpan)
-      } else if (emailReg.test(e.target.value) == true && e.target.classList.contains('border-red-400')){
-        e.target.classList.remove('border-red-400')
-        e.target.parentElement.removeChild(e.target.parentElement.lastElementChild)
-      }
-    }
 
     fPodcastName.addEventListener('blur', (e) => validateInputPresence(e))
     fPodcastArtist.addEventListener('blur', (e) => validateInputPresence(e))
