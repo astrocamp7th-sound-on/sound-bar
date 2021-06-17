2373ff10e3798bf57ab8bee886c818d8f6c1df50
Alex Chuang <ball16357@gmail.com>
2021-06-17 15:05:47 +0800

    RWD與外觀調整成主色系(blue)

--- app/javascript/player/header.js
+++ app/javascript/player/header.js
@@ -8,7 +8,7 @@ document.addEventListener("turbolinks:load", function () {
   if (openMyFav) {
     let list = storageFav.map(function(episode){
       return `
-      <li class="my-2 px-2 pb-2 border-b border-yellow-100 hover:bg-yellow-100 hover:text-indigo overflow-hidden textOverflow">
+      <li class="p-2 border-b hover:bg-blue-300 hover:text-indigo overflow-hidden textOverflow">
         <a href="${episode.url}" class="block overflow-hidden textOverflow">${episode.artist} | ${episode.title}</a>
       </li>
       `
